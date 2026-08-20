93bcfb53a5bb445b5115c77c1dab523dabc0aa25
Harendra Godara <harendragodara880@gmail.com>
2026-08-20 14:04:28 +0530

    Learn process and arguments basics

---
+++ 01-process_arguments/args.js
@@ -0,0 +1,12 @@
+/** 
+    ->  process.argv[0]: The absolute path to the Node.js executable. 
+    ->  process.argv[1]: The absolute path to the JavaScript file being executed. 
+    ->  process.argv[2] onwards: The actual arguments provided by the user. 
+*/
+
+// console.log(process.argv);
+
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+
