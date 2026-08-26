7ddd7d841e6c3c7d458d38e7c7ca81beea423b10
Harendra Godara <harendragodara880@gmail.com>
2026-08-27 01:53:55 +0530

    today i learn express

---
+++ 04-httpServer/Express/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+// const http = require("http");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.end("Hello from home page.");
+});
+
+app.get("/about", (req, res) => {
+  res.end(`Hello ${req.query.name}`);
+});
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000, () => {
+//   console.log("Server Started!");
+// });
+
+app.listen(8000, () => {
+  console.log("Server Started!");
+});
--- 04-httpServer/index.js
+++ 04-httpServer/index.js
@@ -7,7 +7,7 @@ const myServer = http.createServer((req, res) => {
   // console.log(req.headers);
   // console.log(req);
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${new Date()} ${req.url} : New Req Received\n`;
+  const log = `${new Date()} ${req.method} ${req.url} : New Req Received\n`;
   const myUrl = url.parse(req.url, true);
   console.log(myUrl);
   fs.appendFile("./log.txt", log, (err, data) => {
@@ -19,6 +19,13 @@ const myServer = http.createServer((req, res) => {
       case "/about":
         const username = myUrl.query.name;
         res.end(`Hey my name is ${username}`);
+        break;
+
+      case "/sinup":
+        if (req.method === "GET") res.end("This is Sinup Form");
+        else if (req.method === "POST")
+          // DB Query
+          res.end("Seccess");
         break;
 
       default:
